816e923707b83f3044bea029461c9e29d5538f15
Madeline Leano <mleano@mit.edu>
2024-04-03 14:25:10 -0400

    so far

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,7 +1,201 @@
+<script>
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
+
+  let data = [];
+  let commits = [];
+  let margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  let cursor = { x: 0, y: 0 };
+  let svg;
+
+  let width = 1000,
+    height = 600;
+
+  let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+  };
+  usableArea.width = usableArea.right - usableArea.left;
+  usableArea.height = usableArea.bottom - usableArea.top;
+
+  let yAxisGridlines;
+
+  onMount(async () => {
+    data = await d3.csv("loc.csv", (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+
+    commits = d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+          id: commit,
+          url: "https://github.com/madelineleano/my-portfolio" + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+
+        // Like ret.lines = lines
+        // but non-enumerable so it doesn’t show up in JSON.stringify
+        Object.defineProperty(ret, "lines", {
+          value: lines,
+          configurable: true,
+          writable: true,
+          enumerable: false,
+        });
+
+        return ret;
+      });
+  });
+
+  $: yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([usableArea.bottom, usableArea.top]);
+  $: xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  let xAxis, yAxis;
+
+  $: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(
+      d3
+        .axisLeft(yScale)
+        .tickFormat((d) => String(d % 24).padStart(2, "0") + ":00")
+    );
+  }
+
+  $: {
+    d3.select(yAxisGridlines).call(
+      d3.axisLeft(yScale).tickFormat("").tickSize(-usableArea.width)
+    );
+  }
+
+  let hoveredIndex = -1;
+  $: hoveredCommit = commits[hoveredIndex] ?? {};
+
+</script>
+
 <svelte:head>
   <title>Meta</title>
 </svelte:head>
 
 <h1>Meta</h1>
 
+<dl
+  id="commit-tooltip"
+  class="info tooltip"
+  hidden={hoveredIndex === -1}
+  style="top: {cursor.y}px; left: {cursor.x}px"
+>
+  <dt>Commit</dt>
+  <dd><a href={hoveredCommit.url} target="_blank">{hoveredCommit.id}</a></dd>
+
+  <dt>Date</dt>
+  <dd>{hoveredCommit.datetime?.toLocaleString("en", { date: "full" })}</dd>
+
+  <dt>Author</dt>
+  <dd>{hoveredCommit.author}</dd>
+  <!-- Add: Time, author, lines edited -->
+
+</dl>
+
+<svg viewBox="0 0 {width} {height}">
+  <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+  <g
+    class="gridlines"
+    transform="translate({usableArea.left}, 0)"
+    bind:this={yAxisGridlines}
+  />
+
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+  <g class="dots">
+    {#each commits as commit, index}
+      <circle
+        cx={xScale(commit.datetime)}
+        cy={yScale(commit.hourFrac)}
+        r="5"
+        fill="steelblue"
+        on:mouseenter={(evt) => {
+          hoveredIndex = index;
+          cursor = { x: evt.x, y: evt.y };
+        }}
+        on:mouseleave={(evt) => (hoveredIndex = -1)}
+      />
+    {/each}
+  </g>
+</svg>
+
 <p>info</p>
+
+<dl class="stats">
+  <dt>Total Lines of Code:</dt>
+  <dd>{data.length}</dd>
+  <dt>Total commits:</dt>
+  <dd>{commits.length}</dd>
+  <dt>Total Files:</dt>
+  <dd>{d3.group(data, (d) => d.file).size}</dd>
+  <dt>Average depth of line:</dt>
+  <dd>{d3.mean(data, (d) => d.depth)}</dd>
+  <dt>Maximum depth of line:</dt>
+  <dd>{d3.max(data, (d) => d.depth)}</dd>
+</dl>
+
+<style>
+  svg {
+    overflow: visible;
+  }
+
+  .gridlines {
+    stroke-opacity: 0.2;
+  }
+
+  dl.info {
+    display: grid;
+    transition-duration: 500ms;
+    transition-property: opacity, visibility;
+
+    &[hidden]:not(:hover, :focus-within) {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+
+  .tooltip {
+    position: fixed;
+    top: 1em;
+    left: 1em;
+    background-color: oklch(100% 0% 0 / 80%);
+    border-radius: 3px;
+    box-shadow: 10px 5px 5px grey;
+    backdrop-filter: blur(2px);
+  }
+
+  circle {
+    transition: 200ms;
+    transform-origin: center;
+    transform-box: fill-box;
+
+    &:hover {
+      transform: scale(1.5);
+    }
+  }
+</style>
